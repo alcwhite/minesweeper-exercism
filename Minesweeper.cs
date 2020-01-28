d59d9814fb485595834354035cd080afa0d7856c
Al White <alcwhite13@gmail.com>
2020-01-28 17:01:12 -0500

    Initial commit; finds cells with mines

---
+++ Minesweeper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Minesweeper
+{
+    public static string[] Annotate(string[] input)
+    {
+        var rowsWithMines = new List<int>();
+        input.Where((s, i) => 
+        {
+            if(s.Contains('*')) 
+                rowsWithMines.Add(i);
+            return true;
+        });
+        var allCells = input.Select(x => x.ToCharArray()).ToList();
+        var cellsWithMines = new List<(int row, int column)>();
+        rowsWithMines.ForEach(x => 
+        {
+            allCells[x].Where((y, i) => 
+            {
+                if (y == '*')
+                    cellsWithMines.Add((x, i));
+                return true;
+            });
+        });
+        var rows = input.Length;
+        return input == Array.Empty<string>() ? Array.Empty<string>() : allCells.Select(x => new string(x)).ToArray();
+        
+    }
+}
